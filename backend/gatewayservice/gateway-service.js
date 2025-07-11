903207357f88aeec12994e66361df3ab60441207
iyanfdezz <uo288231@uniovi.es>
2025-07-11 11:18:32 +0200

    now larger images can be updated

--- backend/gatewayservice/gateway-service.js
+++ backend/gatewayservice/gateway-service.js
@@ -13,11 +13,9 @@ const locationServiceUrl = process.env.LOCATION_SERVICE_URL || "http://localhost
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ limit: '1mb', extended: true }));
-
-// Límites aumentados solo para rutas con imágenes
+// Límites aumentados solo para rutas con imágenes o payloads grandes
 const largePayloadMiddleware = express.json({ limit: '50mb' });
 
 app.set("json spaces", 40);
